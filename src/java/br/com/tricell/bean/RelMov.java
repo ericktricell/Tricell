0588d8da54d8efb895c94dcf487e31de1f64f389
Erick Alves <re.erick86@gmail.com>
2018-03-24 18:03:42 -0300

    inicial

---
+++ src/java/br/com/tricell/bean/RelMov.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.tricell.bean;
+
+import br.com.tricell.dao.MovimentoJpaController;
+import br.com.tricell.model.Movimento;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.persistence.EntityManagerFactory;
+
+/**
+ *
+ * @author Eu
+ */
+@ManagedBean
+@SessionScoped
+public class RelMov {
+    private EntityManagerFactory emf;
+    private Movimento mov = new Movimento();
+    private List<Movimento> lsMov;
+
+    public void pegaTodos(){
+        lsMov = new MovimentoJpaController(emf).findMovimentoEntities();
+    }
+    
+    public EntityManagerFactory getEmf() {
+        return emf;
+    }
+
+    public void setEmf(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+
+    public Movimento getMov() {
+        return mov;
+    }
+
+    public void setMov(Movimento mov) {
+        this.mov = mov;
+    }
+
+    public List<Movimento> getLsMov() {
+        return lsMov;
+    }
+
+    public void setLsMov(List<Movimento> lsMov) {
+        this.lsMov = lsMov;
+    }
+    
+    
+}
